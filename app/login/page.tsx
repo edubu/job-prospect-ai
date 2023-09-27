bc47ffec07954499c3ee31fd36ca5e011233fbd5
edubu <edubu@umich.edu>
2023-09-26 20:19:59 -0400

    testing auth outside of localhost

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -8,6 +8,18 @@ import { ThemeSupa } from "@supabase/auth-ui-shared";
 import { useRouter } from "next/navigation";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
+const getURL = () => {
+  let url =
+    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    "http://localhost:3000/";
+  // Make sure to include `https://` when not localhost.
+  url = url.includes("http") ? url : `https://${url}`;
+  // Make sure to include a trailing `/`.
+  url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
+  return url;
+};
+
 const LoginPage: React.FC = () => {
   const router = useRouter();
 
@@ -36,7 +48,7 @@ const LoginPage: React.FC = () => {
         supabaseClient={supabase}
         providers={["google"]}
         appearance={{ theme: ThemeSupa }}
-        // redirectTo={"https://jobprospect.ai/auth/callback"}
+        redirectTo={getURL()}
       />
     </div>
   );
